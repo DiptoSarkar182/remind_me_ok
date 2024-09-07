322ad086d3ad202b9ff9bc32062711081a118a67
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-09-07 23:25:17 +0600

    added reminder job destroy

--- app/controllers/remind_mes_controller.rb
+++ app/controllers/remind_mes_controller.rb
@@ -1,5 +1,4 @@
 class RemindMesController < ApplicationController
-
   before_action :authenticate_user!
   before_action :set_remind_me, only: [:edit, :update, :destroy]
   before_action :authorize_user!, only: [:edit, :update, :destroy]
@@ -12,12 +11,18 @@ class RemindMesController < ApplicationController
   def create
     @remind_me = current_user.remind_mes.build(remind_me_params)
 
-    if current_user.time_zone.blank? || current_user.time_zone != params[:remind_me][:time_zone]
-      current_user.update(time_zone: params[:remind_me][:time_zone])
+    if current_user.time_zone.blank? || current_user.time_zone != params[:remind_me][:remind_me_time_zone]
+      current_user.update(time_zone: params[:remind_me][:remind_me_time_zone])
     end
 
+    # Convert the remind_me_date_time to UTC based on user's time zone
+    reminder_time_in_utc = remind_me_params[:remind_me_date_time].in_time_zone(params[:remind_me][:remind_me_time_zone]).utc
+    @remind_me.remind_me_date_time = reminder_time_in_utc
+
     if @remind_me.save
-      ReminderJob.set(wait_until: @remind_me.remind_me_date_time).perform_later(@remind_me)
+      # Schedule the job at the converted UTC time and store the job ID
+      job = ReminderJob.set(wait_until: reminder_time_in_utc).perform_later(@remind_me)
+      @remind_me.update(job_id: job.provider_job_id)
       redirect_to dashboards_path, notice: "Reminder created successfully"
     else
       render 'new', status: :unprocessable_entity
@@ -28,8 +33,8 @@ class RemindMesController < ApplicationController
   end
 
   def update
-    if current_user.time_zone.blank? || current_user.time_zone != params[:remind_me][:time_zone]
-      current_user.update(time_zone: params[:remind_me][:time_zone])
+    if current_user.time_zone.blank? || current_user.time_zone != params[:remind_me][:remind_me_time_zone]
+      current_user.update(time_zone: params[:remind_me][:remind_me_time_zone])
     end
 
     if @remind_me.update(remind_me_params)
@@ -40,7 +45,22 @@ class RemindMesController < ApplicationController
   end
 
   def destroy
-    @remind_me.destroy
+    ActiveRecord::Base.transaction do
+      # Lock the reminder record to prevent race conditions
+      @remind_me.lock!
+
+      # Find the associated job using the stored job ID
+      job = GoodJob::Job.find_by(active_job_id: @remind_me.job_id)
+
+      # If the job is scheduled and not yet performed, delete it
+      if job && job.scheduled_at > Time.current && job.performed_at.nil?
+        job.destroy
+      end
+
+      # Destroy the reminder
+      @remind_me.destroy
+    end
+
     redirect_to dashboards_path, notice: "Reminder deleted successfully"
   end
 
@@ -55,11 +75,10 @@ class RemindMesController < ApplicationController
   end
 
   def remind_me_params
-    params.require(:remind_me).permit(:remind_me_date_time, :subject, :content)
+    params.require(:remind_me).permit(:remind_me_date_time, :subject, :content, :remind_me_time_zone)
   end
 
   def load_current_user
     @current_user = current_user
   end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_06_125342) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_07_170244) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
@@ -119,6 +119,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_06_125342) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "subject", default: "", null: false
+    t.string "remind_me_time_zone"
+    t.string "job_id", default: "", null: false
     t.index ["user_id"], name: "index_remind_mes_on_user_id"
   end
 
