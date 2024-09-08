39070514a8982e71be42df08920d1f6418095a7e
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-09-08 20:10:29 +0600

    user cannot create and update in past time, user can reschedule already reminded job again, dashboard have will remind and already reminded you.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,8 @@ class DashboardsController < ApplicationController
 
   before_action :authenticate_user!
   def index
-    @remind_mes = current_user.remind_mes
+    current_time = Time.current
+    @reminded_you = current_user.remind_mes.where('remind_me_date_time <= ?', current_time)
+    @will_remind_you = current_user.remind_mes.where('remind_me_date_time > ?', current_time)
   end
 end
