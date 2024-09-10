8ef4b581eac75169988c91e2201867903ec1cc43
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-09-10 22:14:46 +0600

    added encryption for subject and content or reminder

--- app/models/remind_me.rb
+++ app/models/remind_me.rb
@@ -1,3 +1,10 @@
 class RemindMe < ApplicationRecord
   belongs_to :user
+
+  validates :subject, presence: true
+  validates :content, presence: true
+  validates :remind_me_date_time, presence: true
+  validates :remind_me_time_zone, presence: true
+
+  encrypts :subject, :content
 end
