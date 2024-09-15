355a777e4d0274c340349bf2b2fa35c7572dcf6b
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-09-15 21:50:29 +0600

    remind me ok will be dashboard path for edit user registration path, default email added for view use

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,11 @@ module ApplicationHelper
       { controller: 'dashboards', action: 'index' },
       { controller: 'remind_mes', action: 'new' },
       { controller: 'remind_mes', action: 'create' },
+      { controller: 'remind_mes', action: 'show' },
       { controller: 'remind_mes', action: 'edit' },
       { controller: 'remind_mes', action: 'update' },
-      { controller: 'devise/registrations', action: 'edit' }
+      { controller: 'users/registrations', action: 'edit' },
+      { controller: 'users/registrations', action: 'update' }
     ]
 
     pages_to_hide_elements.any? do |page|
@@ -16,5 +18,9 @@ module ApplicationHelper
 
   def dashboard_link_path
     hide_elements_on_pages? ? dashboards_path : root_path
+  end
+
+  def default_email_from
+    ApplicationMailer.default[:from]
   end
 end
